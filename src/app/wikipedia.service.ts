a1854320b57f46289f28792112763503f14c8ee6
Forstie <Ryan.Forstie1@T-Mobile.com>
2020-03-02 13:22:22 -0800

    Add search service

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class WikipediaService {
+  constructor() {}
 
-  constructor() { }
+  search(term: string) {
+    return "I am wiki search results";
+  }
 }
